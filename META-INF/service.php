a7212673c208d1096eb9bba37eff214cfb950edb
xiaohengjin <xiaohengjin@youzan.com>
2017-09-01 18:15:35 +0800

    WSServer instance bind

--- META-INF/service.php
+++ META-INF/service.php
@@ -1,3 +1,11 @@
 <?php
 
+use ZanPHP\Support\Di;
+
+$container = \ZanPHP\Container\Container::getInstance();
+
+$container->bind("ServerBase.WSServer", function ($_, $args) {
+    return Di::make(\ZanPHP\WSServer\Server::class, [$args[0], $args[1]]);
+});
+
 return [];
